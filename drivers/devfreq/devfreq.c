b3068153dd77cf5931d978b94135033da94e5265
andip71 <andreasp@gmx.de>
2015-06-15 09:05:41 +0200

    devfreq: gpu: added simple time_in_state stats
    * Modded by anarkia1976 to be similar to CPU stats table
    
    Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>
    
    apply some changes to devfreq_attrs to fit new kernel versions
    since I'm coming from a 3.10.108 kernel...
    
    Signed-off-by: Pierre2324 <pdbbasketball@gmail.com>

--- drivers/devfreq/devfreq.c
+++ drivers/devfreq/devfreq.c
@@ -1392,6 +1392,27 @@ static ssize_t trans_stat_show(struct device *dev,
 }
 static DEVICE_ATTR_RO(trans_stat);
 
+static ssize_t time_in_state_show(struct device *dev, struct device_attribute *attr,
+                                char *buf)
+{
+	struct devfreq *devfreq = to_devfreq(dev);
+
+        ssize_t len = 0;
+        int i, err;
+		unsigned int max_state = devfreq->profile->max_state;
+
+        err = devfreq_update_status(devfreq, devfreq->previous_freq);
+        if (err)
+                return 0;
+
+		for (i = 0; i < max_state; i++) {
+                len += sprintf(buf + len, "%u %u\n", devfreq->profile->freq_table[i],
+                        jiffies_to_msecs(devfreq->time_in_state[i]));
+        }
+        return len;
+}
+static DEVICE_ATTR_RO(time_in_state);
+
 static struct attribute *devfreq_attrs[] = {
 	&dev_attr_name.attr,
 	&dev_attr_governor.attr,
@@ -1403,6 +1424,7 @@ static struct attribute *devfreq_attrs[] = {
 	&dev_attr_min_freq.attr,
 	&dev_attr_max_freq.attr,
 	&dev_attr_trans_stat.attr,
+	&dev_attr_time_in_state.attr,
 	NULL,
 };
 ATTRIBUTE_GROUPS(devfreq);
