0c109b26dd598dc56794bbc27e0aa46e44ec622b
Pierre2324 <pdbbasketball@gmail.com>
2022-11-17 21:23:46 -0500

    Welcome to NoGravity Kernel for Kona (Xiaomi) devices
    Signed-off-by: Pierre2324 <pdbbasketball@gmail.com>

---
+++ compile-apollo.sh
@@ -0,0 +1,86 @@
+#!/bin/sh
+
+# Some general variables
+ARCH="arm64"
+SUBARCH="arm64"
+DEFCONFIG=nogravity-apollo_defconfig
+COMPILER=clang
+LINKER="lld"
+COMPILERDIR="/media/pierre/Expension/Android/PocoX3Pro/Kernels/Proton-Clang"
+
+# Export shits
+export KBUILD_BUILD_USER=Pierre2324
+export KBUILD_BUILD_HOST=G7-7588
+
+# Speed up build process
+MAKE="./makeparallel"
+
+# Basic build function
+BUILD_START=$(date +"%s")
+blue='\033[0;34m'
+cyan='\033[0;36m'
+yellow='\033[0;33m'
+red='\033[0;31m'
+nocol='\033[0m'
+
+Build () {
+PATH="${COMPILERDIR}/bin:${PATH}" \
+make -j$(nproc --all) O=out \
+ARCH=${ARCH} \
+LLVM=1 \
+LLVM_IAS=1 \
+CC=${COMPILER} \
+CROSS_COMPILE=${COMPILERDIR}/bin/aarch64-linux-gnu- \
+CROSS_COMPILE_COMPAT=${COMPILERDIR}/bin/arm-linux-gnueabi- \
+LD_LIBRARY_PATH=${COMPILERDIR}/lib \
+Image.gz-dtb dtbo.img
+}
+
+Build_lld () {
+PATH="${COMPILERDIR}/bin:${PATH}" \
+make -j$(nproc --all) O=out \
+ARCH=${ARCH} \
+LLVM=1 \
+LLVM_IAS=1 \
+CC=${COMPILER} \
+CROSS_COMPILE=${COMPILERDIR}/bin/aarch64-linux-gnu- \
+CROSS_COMPILE_COMPAT=${COMPILERDIR}/bin/arm-linux-gnueabi- \
+LD=ld.${LINKER} \
+AR=llvm-ar \
+NM=llvm-nm \
+OBJCOPY=llvm-objcopy \
+OBJDUMP=llvm-objdump \
+STRIP=llvm-strip \
+ld-name=${LINKER} \
+KBUILD_COMPILER_STRING="Proton Clang" \
+Image.gz-dtb dtbo.img
+}
+
+# Make defconfig
+
+make O=out ARCH=${ARCH} ${DEFCONFIG}
+if [ $? -ne 0 ]
+then
+    echo "Build failed"
+else
+    echo "Made ${DEFCONFIG}"
+fi
+
+# Build starts here
+if [ -z ${LINKER} ]
+then
+    Build
+else
+    Build_lld
+fi
+
+if [ $? -ne 0 ]
+then
+    echo "Build failed"
+else
+    echo "Build succesful"
+fi
+
+BUILD_END=$(date +"%s")
+DIFF=$(($BUILD_END - $BUILD_START))
+echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
