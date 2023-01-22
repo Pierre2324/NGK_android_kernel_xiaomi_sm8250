b8a5461851358936508a6e8f4e076ac6a089868a
Pierre2324 <pdbbasketball@gmail.com>
2023-01-22 17:16:01 -0500

    Revert "timerqueue: Use rb_entry_safe() in timerqueue_getnext()"
    This reverts commit 6de565ac9f003863f4773b112056ed7fbd3269c4.

--- include/linux/timerqueue.h
+++ include/linux/timerqueue.h
@@ -35,9 +35,7 @@ extern struct timerqueue_node *timerqueue_iterate_next(
 static inline
 struct timerqueue_node *timerqueue_getnext(struct timerqueue_head *head)
 {
-	struct rb_node *leftmost = rb_first_cached(&head->rb_root);
-
-	return rb_entry_safe(leftmost, struct timerqueue_node, node);
+	return head->next;
 }
 
 static inline void timerqueue_init(struct timerqueue_node *node)
